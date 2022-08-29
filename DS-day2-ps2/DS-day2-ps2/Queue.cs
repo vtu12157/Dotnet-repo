54a38a6acb8d0fe89686c2c46b884c940f13cb71
john <john@gmail.com>
2022-08-30 00:14:08 +0530

    dev

--- DS-day2-ps2/DS-day2-ps2/Queue.cs
+++ DS-day2-ps2/DS-day2-ps2/Queue.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DS_day2_ps2
 {
-    // C# program to implement a queue using an array
+   
     using System;
 
     public class Queue
@@ -21,18 +21,17 @@ namespace DS_day2_ps2
             queue = new int[capacity];
         }
 
-        // function to insert an element
-        // at the rear of the queue
+        
         public void Enqueue(int data)
         {
-            // check queue is full or not
+           
             if (capacity == rear)
             {
                 Console.Write("\nQueue is full\n");
                 return;
             }
 
-            // insert element at the rear
+           
             else
             {
                 queue[rear] = data;
@@ -41,19 +40,17 @@ namespace DS_day2_ps2
             return;
         }
 
-        // function to delete an element
-        // from the front of the queue
+        
         public void Dequeue()
         {
-            // if queue is empty
+            
             if (front == rear)
             {
                 Console.Write("\nQueue is empty\n");
                 return;
             }
 
-            // shift all the elements from index 2 till rear
-            // to the right by one
+            
             else
             {
                 for (int i = 0; i < rear - 1; i++)
@@ -61,11 +58,11 @@ namespace DS_day2_ps2
                     queue[i] = queue[i + 1];
                 }
 
-                // store 0 at rear indicating there's no element
+
                 if (rear < capacity)
                     queue[rear] = 0;
 
-                // decrement rear
+               
                 rear--;
             }
             return;
@@ -80,7 +77,7 @@ namespace DS_day2_ps2
                 return;
             }
 
-            // traverse front to rear and print elements
+          
             for (i = front; i < rear; i++)
             {
                 Console.WriteLine( queue[i]);
