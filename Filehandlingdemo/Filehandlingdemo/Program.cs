f662e4beaa588ca7f911015c393374c209048be1
john <john@gmail.com>
2022-08-29 16:35:53 +0530

    c# problem statementscommit

---
+++ Filehandlingdemo/Filehandlingdemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Filehandlingdemo
+{
+    internal class FileStreamsDemo
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("Demo1.txt", FileMode.);
+        }
+    }
+}
