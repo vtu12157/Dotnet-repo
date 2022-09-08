f670a9ac3d431b7b47d220488a06821f7c978462
john <john@gmail.com>
2022-09-08 22:50:38 +0530

    Delegate events

---
+++ deptcls/Library1/Dept.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library1
+{
+    public class Dept
+    {
+        public delegate void AddDelegate(Employee emp);
+
+        public delegate void RemoveDelegate(int empID);
+
+        public delegate void UpdateDelegate(Employee emp);
+
+        public event AddDelegate AddEvent;
+
+        public event RemoveDelegate RemoveEvent;
+
+        public event UpdateDelegate UpdateEvent;
+
+        public Dept()
+        {
+            Employees = new List<Employee>();
+            //registration
+            AddEvent += new AddDelegate(Emp_AddEvent);
+            RemoveEvent += new RemoveDelegate( Emp_RemoveEvent);
+            UpdateEvent += new UpdateDelegate(Emp_UpdateEvent);
+        }
+
+        public void RaiseAddEvent(Employee emp)
+        {
+            AddEvent.Invoke(emp);
+        }
+
+        public void RaiseUpdateEvent(Employee emp)
+        {
+            UpdateEvent.Invoke(emp);
+        }
+
+        public void RaiseDeleteEvent(int empID)
+        {
+            RemoveEvent.Invoke(empID);
+        }
+
+        private void Emp_UpdateEvent(Employee emp)
+        {
+          var obj =  Employees.FirstOrDefault(x => x.Employeeid == emp.Employeeid);
+
+            obj.deptno = emp.deptno;
+            obj.manager =  emp.manager;
+            obj.Employeename = emp.Employeename;
+        }
+
+        private void Emp_RemoveEvent(int empID)
+        {
+            Employees.RemoveAll(  x=> x.Employeeid == empID);
+        }
+
+        private void Emp_AddEvent(Employee emp)
+        {
+            Employees.Add(emp);
+        }
+
+        public int Deptno { get; set; }
+        public string DeptName { get; set; }
+        public string DeptLocation { get; set; }    
+
+        public List<Employee> Employees { get; set; }
+
+    }
+
+    public class Employee
+    {
+        public int Employeeid { get; set; }
+        public string Employeename { get; set; }
+
+        public int deptno { get; set; }
+        public int salary { get; set; } 
+        public string manager { get; set; }
+    }
+    
+    
+}
---
+++ deptcls/deptcls/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library1;
+
+namespace deptcls
+{
+    internal class Program
+    {
+
+        delegate void DisplayEmployees(List<Employee> employees);
+
+        static void Main(string[] args)
+        {
+
+
+
+            Dept dept = new Dept();
+
+            dept.RaiseAddEvent(new Employee() { Employeeid = 1, deptno = 1,Employeename = "Ram" , manager = "Kiran" });
+
+            dept.RaiseAddEvent(new Employee() { Employeeid = 2, deptno = 1, Employeename = "Peter", manager = "Rob" });
+
+            dept.RaiseUpdateEvent(new Employee() { Employeeid = 2, deptno = 1, Employeename = "Peter1", manager = "Rob1" });
+
+            dept.RaiseDeleteEvent(1);
+
+            dept.RaiseAddEvent(new Employee() { Employeeid = 1, deptno = 1, Employeename = "Ram", manager = "Kiran" });
+
+            DisplayEmployees displayEmployees = DisplayEmployees1;
+
+            displayEmployees.Invoke(dept.Employees);
+
+            //func , Action, Predicate
+
+
+            Console.WriteLine("");
+            Action<List<Employee>> action = DisplayEmployeesAndManagers;
+            Func<Employee,bool> predicate = x => x.manager != null;
+
+            action.Invoke(dept.Employees.Where(predicate).ToList());
+            Console.ReadKey();
+
+        }
+
+        public static void DisplayEmployees1(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine( "Name: " + employee.Employeename);
+                Console.WriteLine("Manager: " + employee.manager);
+                Console.WriteLine("DeptNo: " + employee.deptno);
+                Console.WriteLine("ID: " + employee.Employeeid);
+                Console.WriteLine("Salary: " + employee.salary);
+                Console.WriteLine("-----------------------------");
+            }
+
+        }
+
+        public static void DisplayEmployeesAndManagers(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("Name: " + employee.Employeename);
+                Console.WriteLine("Manager: " + employee.manager);
+                Console.WriteLine("-----------------------------");
+            }
+
+        }
+    }
+}
